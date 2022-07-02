42f3ae59abfe1e4a20b49a05096cef9e1e374943
Eding Muhamad <masaruedo@yahoo.com>
2022-07-03 05:58:08 +0700

    Define first routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function () {
+    return "<h1>All contacts</h1>";
+});
+
+Route::get('/contacts/create', function () {
+    return "<h1>Add new contact</h1>";
+});
