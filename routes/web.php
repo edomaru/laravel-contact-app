798a43437796288913c6153014d5a4bf2c3c6fce
Eding Muhamad <masaruedo@yahoo.com>
2022-07-05 20:14:59 +0700

    Add a fallback route

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,18 @@ Route::get('/', function () {
     return $html;
 });
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/contacts', function () {
-        return "<h1>All contacts</h1>";
-    })->name('contacts.index');
+Route::get('/contacts', function () {
+    return "<h1>All contacts</h1>";
+})->name('contacts.index');
 
-    Route::get('/contacts/create', function () {
-        return "<h1>Add new contact</h1>";
-    })->name('contacts.create');
+Route::get('/contacts/create', function () {
+    return "<h1>Add new contact</h1>";
+})->name('contacts.create');
 
-    Route::get('/contacts/{id}', function ($id) {
-        return "Contact " . $id;
-    })->name('contacts.show');
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact " . $id;
+})->name('contacts.show');
+
+Route::fallback(function () {
+    return "<h1>Sorry, the page does not exist</h1>";
 });
