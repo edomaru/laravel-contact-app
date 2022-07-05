d2961a6a43fd63be02ade413626bb8da1e35009e
Eding Muhamad <masaruedo@yahoo.com>
2022-07-05 20:03:59 +0700

    Add names to the routes

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $html = "
+    <h1>Contact App</h1>
+    <div>
+        <a href='" . route('contacts.index') . "'>All contacts</a>
+        <a href='" . route('contacts.create') . "'>Add contact</a>
+        <a href='" . route('contacts.show', 1) . "'>Show contact</a>
+    </div>
+    ";
+    return $html;
 });
 
 Route::get('/contacts', function () {
     return "<h1>All contacts</h1>";
-});
+})->name('contacts.index');
 
 Route::get('/contacts/create', function () {
     return "<h1>Add new contact</h1>";
-});
+})->name('contacts.create');
 
 Route::get('/contacts/{id}', function ($id) {
     return "Contact " . $id;
-})->whereNumber('id');
-
-Route::get('/companies/{name?}', function ($name = null) {
-    if ($name) {
-        return "Company " . $name;
-    } else {
-        return "All companies";
-    }
-})->whereAlphaNumeric('name');
+})->name('contacts.show');
