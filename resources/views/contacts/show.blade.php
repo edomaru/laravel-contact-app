f6c4628b020610ed8be18750e6ad1650d0ff5cf0
Eding Muhamad <masaruedo@yahoo.com>
2022-07-13 09:51:58 +0700

    Another way passing data to view

---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contact details</h1>
+    <div>
+        <p>Name: {{ $contact['name'] }}</p>
+        <p>Phone: {{ $contact['phone'] }}</p>
+    </div>
+    <div>
+        <a href='{{ route('contacts.index') }}'>Back to all contacts</a>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+function getContacts()
+{
+    return [
+        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
+        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
+        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
+    ];
+}
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/contacts', function () {
-    $contacts = [
-        1 => ['name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['name' => 'Name 3', 'phone' => '3456789012'],
-    ];
+    $contacts = getContacts();
     return view('contacts.index', compact('contacts'));
 })->name('contacts.index');
 
@@ -31,5 +36,8 @@ Route::get('/contacts/create', function () {
 })->name('contacts.create');
 
 Route::get('/contacts/{id}', function ($id) {
-    return "Contact " . $id;
+    $contacts = getContacts();
+    abort_unless(isset($contacts[$id]), 404);
+    $contact = $contacts[$id];
+    return view('contacts.show')->with('contact', $contact);
 })->name('contacts.show');
