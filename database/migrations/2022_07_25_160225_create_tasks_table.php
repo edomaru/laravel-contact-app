f52537cc50885acef59b222d8358027bd980a171
Eding Muhamad <masaruedo@yahoo.com>
2022-07-25 23:07:28 +0700

    Crate migration for defining the Tasks table schema

---
+++ database/migrations/2022_07_25_160225_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->date('due_at')->nullable();
+            $table->boolean('status')->nullable()->comment('NULL: Not started, FALSE: In progress, TRUE: completed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
