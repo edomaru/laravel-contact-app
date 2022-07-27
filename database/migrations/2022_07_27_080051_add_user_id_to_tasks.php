54da5726f290aaa00d96b46fd41b1e08ff160047
Eding Muhamad <masaruedo@yahoo.com>
2022-07-27 22:43:41 +0700

    Remove the foreign key

--- database/migrations/2022_07_27_080051_add_user_id_to_tasks.php
+++ database/migrations/2022_07_27_080051_add_user_id_to_tasks.php
@@ -27,6 +27,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
             $table->dropColumn('user_id');
         });
     }
