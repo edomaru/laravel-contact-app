5ec88df9ce504bcec48f2c858ec7f03871da9aa6
Eding Muhamad <masaruedo@yahoo.com>
2022-08-19 21:36:19 +0700

    Create Contact factory

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'phone' => fake()->phoneNumber(),
+            'email' => fake()->email(),
+            'address' => fake()->address(),
+            'company_id' => Company::pluck('id')->random()
+        ];
+    }
+}
