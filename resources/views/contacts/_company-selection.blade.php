fb1d0160303199f5261276b5297792274eaed354
Eding Muhamad <masaruedo@yahoo.com>
2022-09-03 19:55:54 +0700

    Handling search data on the client side

--- resources/views/contacts/_company-selection.blade.php
+++ resources/views/contacts/_company-selection.blade.php
@@ -1,8 +1,6 @@
-<form method="GET">
-    <select class="custom-select" name="company_id" onchange="this.form.submit()">
-        <option value="" selected>All Companies</option>
-        @foreach ($companies as $id => $name)
-            <option value="{{ $id }}" @if($id == request()->query("company_id")) selected @endif>{{ $name }}</option>
-        @endforeach
-    </select>
-</form>
+<select class="custom-select" name="company_id" id="search-select" onchange="this.form.submit()">
+    <option value="" selected>All Companies</option>
+    @foreach ($companies as $id => $name)
+        <option value="{{ $id }}" @if($id == request()->query("company_id")) selected @endif>{{ $name }}</option>
+    @endforeach
+</select>
--- resources/views/contacts/_filter.blade.php
+++ resources/views/contacts/_filter.blade.php
@@ -1,23 +1,27 @@
 <div class="row">
     <div class="col-md-6"></div>
     <div class="col-md-6">
-      <div class="row">
-        <div class="col">
-          @includeUnless(empty($companies), 'contacts._company-selection')
-        </div>
-        <div class="col">
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button">
-                    <i class="fa fa-refresh"></i>
+      <form>
+        <div class="row">
+          <div class="col">
+            @includeUnless(empty($companies), 'contacts._company-selection')
+          </div>
+          <div class="col">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" name="search" value="{{ request()->query('search') }}" id="search-input" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
+              <div class="input-group-append">
+                @if(request()->filled('search') || request()->filled('company_id'))
+                  <button class="btn btn-outline-secondary" type="button" onclick="document.getElementById('search-input').value = '', document.getElementById('search-select').selectedIndex = 0, this.form.submit()">
+                      <i class="fa fa-refresh"></i>
                   </button>
-              <button class="btn btn-outline-secondary" type="button" id="button-addon2">
-                <i class="fa fa-search"></i>
-              </button>
+                @endif
+                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
+                  <i class="fa fa-search"></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
