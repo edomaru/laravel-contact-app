068a5b834d54966197e102438bf81b011f5cd7a0
Eding Muhamad <masaruedo@yahoo.com>
2022-09-10 22:53:09 +0700

    Setup soft deletion for Company and Contact models

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
     protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address', 'company_id'];
 
     public function company()
