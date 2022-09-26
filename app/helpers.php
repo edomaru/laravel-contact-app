63dcc61fb18d915442b995cfce1f491ada53cee0
Eding Muhamad <masaruedo@yahoo.com>
2022-09-26 19:03:52 +0700

    Styling the sortable columns

--- app/helpers.php
+++ app/helpers.php
@@ -6,7 +6,13 @@ function sortable($label, $column = null)
 {
     $column = $column ?? Str::snake($label);
     $sortBy = request()->query('sort_by');
+    $direction = "";
+    if (ltrim($sortBy, '-') === $column) {
+        $direction = strpos($sortBy, '-') === 0 ? "desc" : "asc";
+    }
+
     $sortBy = !$sortBy || strpos($sortBy, "-") === 0 ? $column : "-{$column}";
     $url = request()->fullUrlWithQuery(['sort_by' => $sortBy]);
-    return "<a href='{$url}'>{$label}</a>";
+
+    return "<a href='{$url}' class='sortable {$direction}'>{$label}</a>";
 }
