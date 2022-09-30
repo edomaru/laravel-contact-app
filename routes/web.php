086cb4454445682eea846e5f196d71a395ace88a
Eding Muhamad <masaruedo@yahoo.com>
2022-10-01 06:22:12 +0700

    implicit bindings

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', WelcomeController::class);
 Route::resource('/contacts', ContactController::class);
-Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])->name('contacts.restore');
-Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])->name('contacts.force-delete');
+Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])
+    ->name('contacts.restore')
+    ->withTrashed();
+Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])
+    ->name('contacts.force-delete')
+    ->withTrashed();
 Route::resource('/companies', CompanyController::class);
 Route::resources([
     '/tags' => TagController::class,
