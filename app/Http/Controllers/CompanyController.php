ea1a86144a30cb18561167a4ff201ca68972a0f3
Eding Muhamad <masaruedo@yahoo.com>
2022-10-16 22:15:55 +0700

    Add a show company feature

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -56,9 +56,9 @@ class CompanyController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Company $company)
     {
-        //
+        return view('companies.show', compact('company'));
     }
 
     /**
