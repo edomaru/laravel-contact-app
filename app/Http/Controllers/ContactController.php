3d42a19355a26c4d2cb26f76298346d7f084216d
Eding Muhamad <masaruedo@yahoo.com>
2022-11-08 08:13:36 +0700

    Move validation logic into form request class

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use App\Repositories\CompanyRepository;
 use Illuminate\Http\Request;
 use App\Models\Contact;
@@ -34,16 +35,8 @@ class ContactController extends Controller
         return view('contacts.create', compact('companies', 'contact'));
     }
 
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
-        $request->validate([
-            'first_name' => 'required|string|max:50',
-            'last_name' => 'required|string|max:50',
-            'email' => 'required|email',
-            'phone' => 'nullable',
-            'address' => 'nullable',
-            'company_id' => 'required|exists:companies,id'
-        ]);
         Contact::create($request->all());
         return redirect()->route('contacts.index')->with('message', 'Contact has been added successfully');
     }
@@ -59,16 +52,8 @@ class ContactController extends Controller
         return view('contacts.edit', compact('companies', 'contact'));
     }
 
-    public function update(Request $request, Contact $contact)
+    public function update(ContactRequest $request, Contact $contact)
     {
-        $request->validate([
-            'first_name' => 'required|string|max:50',
-            'last_name' => 'required|string|max:50',
-            'email' => 'required|email',
-            'phone' => 'nullable',
-            'address' => 'nullable',
-            'company_id' => 'required|exists:companies,id'
-        ]);
         $contact->update($request->all());
         return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
     }
