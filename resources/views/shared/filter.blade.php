24aa3ecbd601c3c8633d8d0930acd210af9027fa
Eding Muhamad <masaruedo@yahoo.com>
2023-01-15 05:12:05 +0700

    Refactor the javascript code

--- resources/views/shared/filter.blade.php
+++ resources/views/shared/filter.blade.php
@@ -20,11 +20,9 @@
             <div class="input-group mb-3">
               <input type="text" class="form-control" name="search" value="{{ request()->query('search') }}" id="search-input" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
               <div class="input-group-append">
-                @if(request()->filled('search') || request()->filled('company_id'))
-                  <button class="btn btn-outline-secondary" type="button" onclick="document.getElementById('search-input').value = '', document.getElementById('search-select').selectedIndex = 0, this.form.submit()">
-                      <i class="fa fa-refresh"></i>
-                  </button>
-                @endif
+                <button class="btn btn-outline-secondary" id="reset-filter-btn" type="button">
+                    <i class="fa fa-refresh"></i>
+                </button>
                 <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
                   <i class="fa fa-search"></i>
                 </button>
@@ -35,3 +33,36 @@
       </form>
     </div>
   </div>
+
+  @push('scripts')
+      <script>
+        document.getElementById('reset-filter-btn').addEventListener('click', () => {
+          let input = document.getElementById('search-input'); 
+          let selects = document.querySelectorAll('.search-select');
+
+          if (input) {
+            input.value = "";
+          }
+
+          selects.forEach(select => {
+            select.selectedIndex = 0;
+          });
+
+          window.location.href = window.location.href.split('?')[0]
+        })
+
+        const toggleClearButton = () => {
+          let query = location.search;
+          let pattern = /[?&]search=/;
+          let button = document.getElementById('reset-filter-btn');
+
+          if (pattern.test(query)) {
+            button.style.display = "block";
+          } else {
+            button.style.display = "none";
+          }
+        }
+
+        toggleClearButton();
+      </script>
+  @endpush
