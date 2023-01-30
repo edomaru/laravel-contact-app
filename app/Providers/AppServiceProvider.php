9da059bb25018612ceb6c277077cf672ecbc95d6
Eding Muhamad <masaruedo@yahoo.com>
2023-01-30 09:01:42 +0700

    Listen query event

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFour();
+        // if (config('app.debug')) {
+        //     DB::listen(fn ($query) => Log::info($query->sql, $query->bindings, $query->time));
+        // }
     }
 }
